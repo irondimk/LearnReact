0acc928f326c6d4a65c3ff412ce315b531108618
IronDimk <irondimk@gmail.com>
2020-10-21 03:50:14 +0700

    add module css on header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import './Header.css';
+import classes from './Header.module.css';
+
 
 function Header() {
     return (
         <header>
-            <div className="wrapper">
-                <img src="http://aimtechmyanmar.com/assets/img/logo/wifi/wifiprojectmanagement.png"></img>
+            <div className={classes.wrapper}>
+                <img className={classes.image} src="http://aimtechmyanmar.com/assets/img/logo/wifi/wifiprojectmanagement.png"></img>
             </div>
         </header>
     );
