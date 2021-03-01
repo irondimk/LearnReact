efd733319d1a5a6248fc9888be8b541550fb9fb6
IronDimk <irondimk@gmail.com>
2021-03-02 04:25:44 +0700

    add validation sign in input add visual update

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -2,7 +2,8 @@ import { render } from '@testing-library/react';
 import React from 'react';
 import { Form, Field } from 'react-final-form';
 import { userAPI } from '../../api/api';
-
+import { composeValidators, maxCount, required } from '../../utils/validators';
+import classes from './Login.module.css';
 
 
 class Login extends React.Component {
@@ -16,20 +17,40 @@ class Login extends React.Component {
     render() {
         return (
             <div>
-                <h1>Login</h1>
+                <h2 className={classes.title}>Sign in</h2>
+
                 <Form
+               
                     onSubmit={this.onSubmit}
                     render={({ handleSubmit, submitting, pristine }) => (
-                        <form onSubmit={handleSubmit}>
-                            <div><Field placeholder={"Login"} name={"login"} component={"input"} id={"Login"} /></div>
-                            <div><Field placeholder={"Password"} name={"password"} component={"input"} /></div>
+                        <form onSubmit={handleSubmit}  className = {classes.form}>
                             <div>
+                            <Field name={"login"} id={"Login"} validate={composeValidators(required, maxCount(30))} >
+                            {({ input, meta }) => (
+              <div className={classes.inputDiv}>
+              <label className={classes.inputLabel}>Email</label>
+                <input {...input} className = {classes.input} type="text" />
+                {meta.error && meta.touched && <span className = {classes.errorSpan}>{meta.error}</span>}
+              </div>
+            )}
+                            </Field>
+                            </div>
+                            <Field name={"password"} id={"password"} validate={composeValidators(required, maxCount(30))} >
+                            {({ input, meta }) => (
+              <div className={classes.inputDiv}>
+              <label className={classes.inputLabel}>Password</label>
+                <input {...input} className = {classes.input} type="password" />
+                {meta.error && meta.touched && <span className = {classes.errorSpan}>{meta.error}</span>}
+              </div>
+            )}
+                            </Field>
+                            <div className={classes.inputDiv}>
                                 <span>
                                     <Field id={"rememberMeCheckbox"} type={"checkbox"} name={"rememberme"} component={"input"} />
                                     <label for={"rememberMeCheckbox"}>Remember me</label>
                                 </span>
                             </div>
-                            <div><button type="submit" disabled={submitting || pristine}>
+                            <div><button className={classes.submit} type="submit" disabled={submitting || pristine}>
                                 Submit
                 </button></div>
                         </form>
