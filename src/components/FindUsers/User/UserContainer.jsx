aeb30bc845647f7ca741eca719ad13d624bf18a1
IronDimk <irondimk@gmail.com>
2021-03-08 07:15:43 +0700

    repair unfollow user button

--- src/components/FindUsers/User/UserContainer.jsx
+++ src/components/FindUsers/User/UserContainer.jsx
@@ -4,9 +4,6 @@ import User from './User';
 import {toggleIsFollowingProcessAC, follow, unfollow} from './../../../redux/users-reducer';
 
 let UsersContainer = (props) => {
-    
-
-
     return(
         <User isFollowingProcess={props.isFollowingProcess} id={props.id} photos={props.photos} name={props.name} status={props.status} followed={props.followed} unfollow={props.unfollow} follow={props.follow} avatarSrc={'https://im0-tub-ru.yandex.net/i?id=0ea34b256aa4cadbdf4631f375db89c7&n=13&exp=1'}/>
     )
@@ -18,7 +15,6 @@ const mapStateToProps = (state) => {
     }
 }
 
-export default connect(mapStateToProps, {toggleIsFollowingProcessAC, follow})(UsersContainer);
+export default connect(mapStateToProps, {toggleIsFollowingProcessAC, follow, unfollow})(UsersContainer);
 
-// export default UsersContainer;
 
