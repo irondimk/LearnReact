66f61087aa1675e7589982a6271960b9c5a3013e
IronDimk <irondimk@gmail.com>
2021-03-09 13:14:25 +0700

    repair profilestatus and use useeffect hook for fix

--- src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
+++ src/components/Profile/ProfileInfo/ProfileStatus/ProfileStatusWithHooks.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import classes from '../ProfileInfo.module.css';
 
 
@@ -7,6 +7,10 @@ const ProfileStatusWithHooks = (props) =>  {
     let [editMode, setEditMode] = useState(false);
     let [status, setStatus] = useState(props.status);
     
+    useEffect( () => {
+        setStatus(props.status)
+    }, [props.status])
+
     const activateEditMode = () => {
         setEditMode(true);
     }
