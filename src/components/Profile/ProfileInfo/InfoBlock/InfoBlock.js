0134c0ac0a0d8257625867f9ba35d4df35174b07
IronDimk <irondimk@gmail.com>
2021-04-02 20:19:48 +0700

    show edit link to infoblock with mouseover to block

--- src/components/Profile/ProfileInfo/InfoBlock/InfoBlock.js
+++ src/components/Profile/ProfileInfo/InfoBlock/InfoBlock.js
@@ -1,12 +1,19 @@
 import classes from './InfoBlock.module.css';
-import React from 'react';
+import React, {useState} from 'react';
 import { NavLink } from 'react-router-dom';
 
-
 let InfoBlock = (props) => {
+
+    let [isShowLinkEdit, setShowLinkEdit] = useState(false);
+
     return (
-        <div className={classes.infoBlock}>
-          {props.canEditProfile ? <NavLink to="settings" className={classes.editProfileLink}>Edit</NavLink> : <div></div>}
+        <div className={classes.infoBlock} 
+        onMouseOver={props.canEditProfile ? () => setShowLinkEdit(true): null} 
+        onMouseOut={props.canEditProfile ? () => setShowLinkEdit(false): null}>
+
+          {isShowLinkEdit ? 
+          <NavLink to="settings" className={classes.editProfileLink}>Edit</NavLink> : 
+          null}
           <h3 className={classes.infoTittle}>{props.title}</h3>
           <div className={classes.content}>
             {props.content}
