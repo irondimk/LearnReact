745bd5c4d515859941e6cefb270d1a7a71492366
IronDimk <irondimk@gmail.com>
2021-04-05 17:17:41 +0700

    delete user from dialogspage

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,13 +1,11 @@
 import React from 'react';
 import classes from './Dialogs.module.css';
 import MessagesContainer from './Messages/MessagesContainer.jsx';
-import UsersContainer from './Users/UsersContainer.jsx';
 const Dialogs = (props) => {
         return (
             <div>
                 <h2>Dialogs</h2>
                 <div className={classes.content}>
-                    <UsersContainer/>
                     <MessagesContainer/>
                 </div>
             </div>
