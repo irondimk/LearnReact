d9f2db76ddc7803f33782408131af907c0efd26a
IronDimk <irondimk@gmail.com>
2021-04-05 18:10:12 +0700

    merge done

--- src/components/Posts/Posts.jsx
+++ src/components/Posts/Posts.jsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import classes from './Posts.module.css';
-<<<<<<< HEAD:src/components/Posts/Posts.jsx
-import MessagesContainer from './Posts/PostsContainer.jsx';
-=======
 import MessagesContainer from './Messages/MessagesContainer.jsx';
->>>>>>> master:src/components/Dialogs/Dialogs.jsx
 const Posts = (props) => {
         return (
             <div>
