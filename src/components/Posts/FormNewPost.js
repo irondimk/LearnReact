610dc05cdf2bd8f6e780b1073db9796dad8f1d1d
IronDimk <irondimk@gmail.com>
2021-04-14 06:18:11 +0700

    add reset to field after submit

--- src/components/Posts/FormNewPost.js
+++ src/components/Posts/FormNewPost.js
@@ -12,45 +12,32 @@ let FormNewPost = (props) => {
         props.sendMessage(value.textMessage);
     };
 
-    const { persistDecorator, clear } = createPersistDecorator({
-        name: "myPersistKey",
-        debounceTime: 500,
-        whitelist: ["textMessage"]
-      });
-      const initialValues = {
-        name: "Hello"
-      };
-
     return (
         <Form
-                decorators={[persistDecorator]}
-                    onSubmit={onSubmit}
-                    render={({ handleSubmit, submitting, pristine, reset, form }) => (
-                        <form
-                            onSubmit={handleSubmit}   
-                        >
-                        <FormStateToRedux form="message" />
-                            <div>
-                            <Field validate={composeValidators(required, maxCount(50))}  name={"textMessage"} id={"textMessage"} >
+            onSubmit={onSubmit}
+            render={({ handleSubmit, reset, submitting, pristine, form }) => (
+                <form
+                     onSubmit={handleSubmit}  
+                >
+                    <FormStateToRedux form="message" />
+                    <div>
+                        <Field validate={composeValidators(required, maxCount(50))} name={"textMessage"} id={"textMessage"} >
                             {({ input, meta }) => (
                                 <div>
-                                    <textarea {...input} className={(meta.error && meta.touched) ? classes.newMessageBoxError : classes.newMessageBox} type="text" placeholder={"Your message"} />
-                                    {meta.error && meta.touched && <span className={classes.spanError}>{meta.error}</span>}
+                                    <textarea {...input} className={classes.newMessageBox} type="text" placeholder={"Your message"} />
                                 </div>
-                                )}
-                            
-                            </Field>
-                            </div><div><button className={classes.submitButton} type="submit" disabled={submitting || pristine}>
-                                    Send
-                </button>
+                            )}
 
-                                
-                            </div>
-                        </form>
-
-
-                    )}
-                />
+                        </Field>
+                    </div>
+                    <div>
+                        <button className={classes.submitButton} onClick={()=> { handleSubmit(); form.reset();}} type="submit" disabled={submitting || pristine}>
+                            Send
+                        </button>
+                    </div>
+                </form>
+            )}
+        />
     )
 }
 
